5e37f133b7c2cfa3313432eebbeac4f0c6ba432e
taovoext-yubico <tao.vo_ext@yubico.com>
2025-09-21 09:45:36 +0700

    update-firebase

--- src/firebase.js
+++ src/firebase.js
@@ -8,15 +8,14 @@ import {
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  databaseURL:
-    'https://app-chat-real-default-rtdb.asia-southeast1.firebasedatabase.app/',
-  apiKey: 'AIzaSyBXqWk6Y4EtHaOm-aWLgeR2rWcdBB6zPxU',
-  authDomain: 'app-chat-real-e2f55.firebaseapp.com',
-  projectId: 'app-chat-real',
-  storageBucket: 'app-chat-real.firebasestorage.app',
-  messagingSenderId: '523159824293',
-  appId: '1:523159824293:web:8ba79fa565a0cd3171ea4c',
-  measurementId: 'G-VDD9TCBNK7',
+  databaseURL: 'https://ecommerce-1b95c-default-rtdb.firebaseio.com',
+  apiKey: 'AIzaSyC7k4OUUIXx7VLOYfMs-RoHG94Xw0dd1hs',
+  authDomain: 'ecommerce-1b95c.firebaseapp.com',
+  projectId: 'ecommerce-1b95c',
+  storageBucket: 'ecommerce-1b95c.firebasestorage.app',
+  messagingSenderId: '1007919859901',
+  appId: '1:1007919859901:web:5d98e7aaa220dbc64d0fd4',
+  measurementId: 'G-088BWM5B3J',
 };
 
 if (!firebaseConfig) {
